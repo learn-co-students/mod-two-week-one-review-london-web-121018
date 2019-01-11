106b0f7f9152bee9297a116e15b6e42717e6810c
github username <github email address>
2019-01-11 14:32:05 +0000

    Sinatra web converted to Rails

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,39 @@
+
+
 class SeasController < ApplicationController
   #define your controller actions here
+
+  def index 
+    @seas = Sea.all
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create 
+    @sea =Sea.create(params[:sea])
+    redirect_to '/seas'
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update_attributes(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def destroy 
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+    redirect_to '/seas'
+  end
 
 
   private
@@ -10,4 +44,5 @@ class SeasController < ApplicationController
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
 
+ 
 end
