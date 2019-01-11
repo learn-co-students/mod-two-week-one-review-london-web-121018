17ad6f7854a2922551a1d91c682af9b04c43c794
seenevz <serneves@hotmail.com>
2019-01-11 12:11:30 +0000

    set up db and basic index route

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,20 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def index
+    @seas = Sea.all
+    render "index.html.erb"
+  end
+
+  def new 
+    render "new.html.erb"
+  end
+
+  def create
+    @sea = Sea.create(params)
+  end
+
+  
 
   private
   # In controller actions, use this private method to access sea params from forms.
